6823ab964591ac834d47c2b9542e92d080dd4fe2
kathosgifre <109618322+kathosgifre@users.noreply.github.com>
2022-10-14 23:30:09 -0400

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,25 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('.imagen-light')
+const hamburger1 = document.querySelector('.hamburger');
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click',()=>{
+        aparecerImagen(imagen.getAttribute('src'));
+    })
+});
+
+contenedorLight.addEventListener('click',(e)=>{
+    if(e.target !== imagenLight){
+        contenedorLight.classList.toggle('show')
+        imagenLight.classList.toggle('showImage')
+        hamburger1.style.opacity = '1';
+    }
+})
+
+const aparecerImagen = (imagen)=>{
+    imagenLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenLight.classList.toggle('showImage')
+    hamburger1.style.opacity = '0';
+}
